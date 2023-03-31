a6c1fe5adb86188929090f902fcdae26bb93ff3e
Julian Victorio <julianv714@gmail.com>
2023-03-30 20:48:13 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+
+function superbowlWin (record) {
+    const yearW = record.find(checkWins)
+    if (yearW) {
+        return yearW.year
+    } 
+    return undefined
+}
+
+function checkWins (recordW) {
+    if (recordW.result === "W")
+        return recordW.year
+}
